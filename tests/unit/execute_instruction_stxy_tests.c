ea8c23bec86803672a49ebe7fe841551529fd1d2
Shock9616 <Shock9616@users.noreply.github.com>
2024-11-20 16:46:04 -0800

    Finish writing tests for STX and STY

--- tests/unit/execute_instruction_stxy_tests.c
+++ tests/unit/execute_instruction_stxy_tests.c
@@ -13,7 +13,7 @@
 Processor processor;
 uint8_t* memory;
 
-void init_stackops_test() {
+void init_stxy_test() {
     // Set registers to default values
     processor.PC = 0x0600;
     processor.S = 0xFF;
@@ -26,128 +26,131 @@ void init_stackops_test() {
     memory = calloc(MEMORY_SPACE, sizeof(uint8_t));
 }
 
-void clean_stackops_test() {
+void clean_stxy_test() {
     free(memory);
 }
 
 // ---------- Tests ----------
 
-void test_instr_pha() {
-    processor.A = 0x69;
-    memory[0x0600] = 0x48;
+void test_instr_stx_zp() {
+    processor.X = 0x69;
+    memory[0x0600] = 0x86;
+    memory[0x0601] = 0x10;
 
     Instruction instr = parseInstruction(memory, processor.PC);
     executeInstruction(instr, &memory, &processor);
     processor.PC += instr.length;
 
-    CU_ASSERT_EQUAL(processor.A, 0x69);
-    CU_ASSERT_EQUAL(memory[0x01FF], 0x69);
+    CU_ASSERT_EQUAL(memory[0x0010], 0x69);
+    CU_ASSERT_EQUAL(processor.P, 0x30);
 }
 
-void test_instr_php() {
-    memory[0x0600] = 0x08;
+void test_instr_stx_zpy() {
+    processor.X = 0x69;
+    processor.Y = 0x05;
+    memory[0x0600] = 0x96;
+    memory[0x0601] = 0x10;
 
     Instruction instr = parseInstruction(memory, processor.PC);
     executeInstruction(instr, &memory, &processor);
     processor.PC += instr.length;
 
-    CU_ASSERT_EQUAL(memory[0x01FF], 0x30);
+    CU_ASSERT_EQUAL(memory[0x0015], 0x69);
+    CU_ASSERT_EQUAL(processor.P, 0x30);
 }
 
-void test_instr_pla() {
-    processor.S = 0xFE;
-    memory[0x01FF] = 0x69;
-    memory[0x0600] = 0x68;
+void test_instr_stx_abs() {
+    processor.X = 0x69;
+    memory[0x0600] = 0x8E;
+    memory[0x0601] = 0x20;
+    memory[0x0602] = 0x10;
 
     Instruction instr = parseInstruction(memory, processor.PC);
     executeInstruction(instr, &memory, &processor);
     processor.PC += instr.length;
 
-    CU_ASSERT_EQUAL(processor.A, 0x69);
-    CU_ASSERT_EQUAL(processor.S, 0xFF);
+    CU_ASSERT_EQUAL(memory[0x1020], 0x69);
+    CU_ASSERT_EQUAL(processor.P, 0x30);
 }
 
-void test_instr_pla_zero() {
-    processor.A = 0x69;
-    processor.S = 0xFE;
-    memory[0x01FF] = 0x00;
-    memory[0x0600] = 0x68;
+void test_instr_sty_zp() {
+    processor.Y = 0x69;
+    memory[0x0600] = 0x84;
+    memory[0x0601] = 0x10;
 
     Instruction instr = parseInstruction(memory, processor.PC);
     executeInstruction(instr, &memory, &processor);
     processor.PC += instr.length;
 
-    CU_ASSERT_EQUAL(processor.A, 0x00);
-    CU_ASSERT_EQUAL(processor.S, 0xFF);
-    CU_ASSERT_EQUAL(processor.P, 0x32);
+    CU_ASSERT_EQUAL(memory[0x0010], 0x69);
+    CU_ASSERT_EQUAL(processor.P, 0x30);
 }
 
-void test_instr_pla_neg() {
-    processor.A = 0x69;
-    processor.S = 0xFE;
-    memory[0x01FF] = 0x96;
-    memory[0x0600] = 0x68;
+void test_instr_sty_zpx() {
+    processor.Y = 0x69;
+    processor.X = 0x05;
+    memory[0x0600] = 0x94;
+    memory[0x0601] = 0x10;
 
     Instruction instr = parseInstruction(memory, processor.PC);
     executeInstruction(instr, &memory, &processor);
     processor.PC += instr.length;
 
-    CU_ASSERT_EQUAL(processor.A, 0x96);
-    CU_ASSERT_EQUAL(processor.S, 0xFF);
-    CU_ASSERT_EQUAL(processor.P, 0xB0);
+    CU_ASSERT_EQUAL(memory[0x0015], 0x69);
+    CU_ASSERT_EQUAL(processor.P, 0x30);
 }
 
-void test_instr_plp() {
-    processor.S = 0xFE;
-    memory[0x01FF] = 0xB1;
-    memory[0x0600] = 0x28;
+void test_instr_sty_abs() {
+    processor.Y = 0x69;
+    memory[0x0600] = 0x8C;
+    memory[0x0601] = 0x20;
+    memory[0x0602] = 0x10;
 
     Instruction instr = parseInstruction(memory, processor.PC);
     executeInstruction(instr, &memory, &processor);
     processor.PC += instr.length;
 
-    CU_ASSERT_EQUAL(processor.P, 0xB1);
-    CU_ASSERT_EQUAL(processor.S, 0xFF);
+    CU_ASSERT_EQUAL(memory[0x1020], 0x69);
+    CU_ASSERT_EQUAL(processor.P, 0x30);
 }
 
 // ---------- Run Tests ----------
 
-CU_pSuite add_stackops_suite_to_registry() {
-    CU_pSuite suite =
-        CU_add_suite_with_setup_and_teardown("executeInstruction Stack Operation Tests", NULL, NULL,
-                                             init_stackops_test, clean_stackops_test);
+CU_pSuite add_stxy_suite_to_registry() {
+    CU_pSuite suite = CU_add_suite_with_setup_and_teardown("executeInstruction STX/STY Tests", NULL,
+                                                           NULL, init_stxy_test, clean_stxy_test);
 
     if (suite == NULL) {
         CU_cleanup_registry();
         return NULL;
     }
 
-    if (CU_add_test(suite, "PHA", test_instr_pha) == NULL) {
+    if (CU_add_test(suite, "STX Zero Page", test_instr_stx_zp) == NULL) {
         CU_cleanup_registry();
         return NULL;
     }
 
-    if (CU_add_test(suite, "PHP", test_instr_php) == NULL) {
+    if (CU_add_test(suite, "STX Zero Page,Y", test_instr_stx_zpy) == NULL) {
         CU_cleanup_registry();
         return NULL;
     }
 
-    if (CU_add_test(suite, "PLA", test_instr_pla) == NULL) {
+    if (CU_add_test(suite, "STX Absolute", test_instr_stx_abs) == NULL) {
         CU_cleanup_registry();
         return NULL;
     }
 
-    if (CU_add_test(suite, "PLA Zero", test_instr_pla_zero) == NULL) {
+    if (CU_add_test(suite, "STY Zero Page", test_instr_sty_zp) == NULL) {
         CU_cleanup_registry();
         return NULL;
     }
 
-    if (CU_add_test(suite, "PLA Negative", test_instr_pla_neg) == NULL) {
+    if (CU_add_test(suite, "STY Zero Page,X", test_instr_sty_zpx) == NULL) {
         CU_cleanup_registry();
         return NULL;
     }
 
-    if (CU_add_test(suite, "PLP", test_instr_plp) == NULL) {
+    if (CU_add_test(suite, "STY Absolute", test_instr_sty_abs) == NULL) {
         CU_cleanup_registry();
         return NULL;
     }
